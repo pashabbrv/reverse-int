30431a5674d4062d1a62e226ce55b8b92cc83c4a
pashabbrv <112647982+pashabbrv@users.noreply.github.com>
2024-09-20 20:57:38 +0300

    feat: create reverse function (Fri, Sep 20, 2024 8:57:41 PM)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let res = 0;
+
+    while(num > 0){
+        res = (res * 10) + (num % 10);
+        num = Math.floor(num / 10);
+    }
+
+    return res;
 }
