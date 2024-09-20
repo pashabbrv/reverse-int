137237249d1a5d9a0a25498147042d9c7ef6823a
pashabbrv <112647982+pashabbrv@users.noreply.github.com>
2024-09-20 21:03:47 +0300

    feat: check negative numbers (Fri, Sep 20, 2024 9:03:51 PM)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-module.exports = function reverse (n) {
+module.exports = function reverse(n) {
     let res = 0;
+    let isNegative = n < 0;
+    n = Math.abs(n);
 
-    while(n > 0){
+    while (n > 0) {
         res = (res * 10) + (n % 10);
         n = Math.floor(n / 10);
     }
 
-    return res;
+    return isNegative ? -res : res;
 }
